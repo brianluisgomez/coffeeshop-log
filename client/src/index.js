e681ae54a42dae110070c0fe5fc6d4552d591a38
Brian <bgome004@gmail.com>
2020-02-14 09:48:06 -0500

    Updates on the client side

--- client/src/index.js
+++ client/src/index.js
@@ -3,4 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.js';
 
+const REACT_APP_MAPBOX_TOKEN =
+  'pk.eyJ1IjoiYmdvbWUwNCIsImEiOiJjazZpYzB1MmEzMzhoM25wanl6azlwN2RuIn0.wDChpRxYI-sSa3dzM-CI1A';
+
 ReactDOM.render(<App />, document.getElementById('root'));
